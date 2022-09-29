95540aa110e50ddef880d971edf186eadcef0d45
Jesiel Viana <jesielviana@proton.me>
2022-09-29 17:01:45 -0300

    configurações iniciais

--- src/App.ts
+++ src/App.ts
@@ -1,14 +1,14 @@
-import express from "express";
+import express from 'express';
 import helmet from 'helmet';
 import routes from './routes/Index';
 
 // criação de uma instância do express/servidor http
-const app = express()
+const app = express();
 app.use(helmet()); // https://helmetjs.github.io/
 
 // middleware para converter o body das requisições para json
 app.use(express.json());
-console.log('kkk')
+console.log('kkk');
 // middleware de tratamento de todas as rotas
 app.use('/', routes);
 
